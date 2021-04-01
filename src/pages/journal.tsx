967709f26e11291e9b33b2bf6642e2a99b931db9
Beau Curnow <curnowbr@gmail.com>
2021-03-31 18:04:17 -0600

    StoryBook Added

--- src/pages/journal.tsx
+++ src/pages/journal.tsx
@@ -1,11 +1,17 @@
 import React, { useRef } from 'react';
 import Editor from "@monaco-editor/react";
+import { useEffect } from 'react';
 
 function Journal() {
 
     const editorRef = useRef(null);
 
-    const [journal, setjournal] = React.useState("Journal");
+    const [journal, setJournal] = React.useState("Journal");
+    const [title, setTitle] = React.useState("Title")
+
+    // useEffect(() => {
+    // })
+
 
     function handleEditorDidMount(editor: any, monaco: any) {
         editorRef.current = editor;
@@ -16,19 +22,21 @@ function Journal() {
             // @ts-ignore: Object is possibly 'null'.
             alert(editorRef.current.getValue());
             // @ts-ignore: Object is possibly 'null'.
-            setjournal(editorRef.current.getValue());
+            setJournal(editorRef.current.getValue());
             console.log(journal)
         }
     }
 
+
     return (
         <div>
-            <div><h3>Journal Title</h3><input defaultValue="Entry Title"></input></div>
+            <div><h3>{title}</h3><input defaultValue={title} onChange={(e) => setTitle(e.target.value)}/></div>
             <div><button onClick={showValue}>Save Journal</button></div>
             <Editor
                 height="90vh"
                 defaultLanguage="javascript"
                 defaultValue="// some comment"
+                value={journal}
                 onMount={handleEditorDidMount}
             />
         </div>
