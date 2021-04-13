2557e765a2585801405ee6d8e5d06b41dd02d13e
Tyler Crowther <tcrowth07@TyBook.local>
2021-04-12 18:04:00 -0600

    register page connected to backend

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,42 +1,63 @@
 import React from "react";
-import Button from '../components/button'
-import Input from '../components/input'
+import Button from "../components/button";
+import Input from "../components/input";
+import { useHistory } from "react-router-dom";
 
 function Login() {
+  let history = useHistory();
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
 
   function handleLogin() {
-    // Put Post Request Here
     console.log({ username, password });
-    fetch("localhost:8080/api/v1/user", {
-      method: "POST",
-      body: JSON.stringify({ username: username, password: password }),
-    });
+    // fetch("localhost:8080/api/v1/user", {
+    //   method: "POST",
+    //   body: JSON.stringify({ username: username, password: password }),
+    // });
+    const fakeResponse = {id: "FOAIENO23OICNM92J", username: "test name", email:"test@test.com", status:200}
+    if (fakeResponse.status === 200) {
+      //store session in cookie
+      //store response in context?
+      history.push("/")
+    } else {
+      console.error(fakeResponse)
+    }
   }
 
   return (
     <div>
       <h1>Please Login</h1>
-      <form onSubmit={handleLogin}>
-        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}><Input
-            type="text"
-            value={username}
-            placeholder="Username"
-            onChange={(e : any) => setUsername(e.target.value)}
-          /></div>
-        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-          <Input
-            type="text"
-            value={password}
-            placeholder="Password"
-            onChange={(e : any) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <Button>Login</Button>
-        </div>
-      </form>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Input
+          type="text"
+          value={username}
+          placeholder="Username"
+          onChange={(e: any) => setUsername(e.target.value)}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Input
+          type="text"
+          value={password}
+          placeholder="Password"
+          onChange={(e: any) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button onClick={() => handleLogin()}>Login</Button>
+      </div>
     </div>
   );
 }
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,52 +1,77 @@
 import React from "react";
+import Button from "../components/button";
+import Input from "../components/input";
+import { useHistory } from "react-router-dom";
 
 function Register() {
   const [username, setUsername] = React.useState("Username");
-
   const [password, setPassword] = React.useState("Password");
-
   const [email, setEmail] = React.useState("Email");
 
-  async function handleRegister() {
-    // Put Post Request Here
-    console.log({ username, password, email });
+  let history = useHistory();
 
-    let res = await fetch("http://localhost:8080/api/v1/user", {
+  async function handleRegister() {
+    await fetch("http://localhost:8080/api/v1/user", {
       method: "POST",
-      body: JSON.stringify( { username: username, email: email } ),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: username, email: email }),
     });
-    console.log(res)
+    history.push("/");
   }
 
   return (
-    <div>
-      <form onSubmit={(e) => handleRegister}>
-        <div>
-          <input
-            placeholder={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            placeholder={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <input placeholder="Confirm Password" />
-        </div>
-        <div>
-          <input
-            placeholder={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <button type="button" onClick={() => handleRegister()}>Login</button>
-        </div>
-      </form>
-    </div>
+    <>
+    <h1>Register</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Input
+          placeholder={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Input
+          placeholder={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Input placeholder="Confirm Password" />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Input placeholder={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <Button type="button" onClick={() => handleRegister()}>
+          Login
+        </Button>
+      </div>
+    </>
   );
 }
 
