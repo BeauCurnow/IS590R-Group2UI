b935fe4707c152cdca6b2639a1a66d4a705cce39
Tyler Crowther <tcrowth07@TyBook.local>
2021-04-13 19:59:00 -0600

    delete entry function works

--- src/pages/entries.tsx
+++ src/pages/entries.tsx
@@ -1,27 +1,33 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+import Button from "../components/button";
 
 function Entries(props: any) {
   let location = useLocation();
-  const [user, setUser] = useState<User>()
+  const [user, setUser] = useState<User>();
   const [entries, setEntries] = useState<Array<JournalEntry>>([]);
 
   useEffect(() => {
-    let user = location.state as User
-    setUser(user)
+    let user = location.state as User;
+    setUser(user);
     fetch("http://localhost:8080/api/v1/journalentry/user/" + user.id)
-    .then((response) => {
+      .then((response) => {
         return response.json();
       })
       .then((data) => {
-        setEntries(data)
+        setEntries(data);
       });
-  },[])
+  }, []);
 
-  function deleteEntry(index: number) {
-    var holder = entries;
-    holder.splice(index, 1);
-    setEntries(holder);
+  function deleteEntry(entryId: string, index: number) {
+    fetch("http://localhost:8080/api/v1/journalentry/" + entryId, {
+      method: "DELETE",
+    }).then((response) => {
+      console.log(response);
+    });
+    let temp = entries.slice();
+    temp.splice(index, 1);
+    setEntries(temp);
   }
 
   var list = entries.map((entry, index) => {
@@ -31,16 +37,16 @@ function Entries(props: any) {
           to={{
             pathname: "/journal",
             state: {
-                id: entry.id,
-                title: entry.title,
-                journal: entry.markdown,
-                user: user
+              id: entry.id,
+              title: entry.title,
+              journal: entry.markdown,
+              user: user,
             },
           }}
         >
           {entry.title}
         </Link>
-        <button onClick={() => deleteEntry(index)}>Delete</button>
+        <Button onClick={() => deleteEntry(entry.id, index)}>Delete</Button>
       </div>
     );
   });
@@ -55,13 +61,13 @@ function Entries(props: any) {
 export default Entries;
 
 interface User {
-    id: string,
-    name: string,
-    email: string
-  }
+  id: string;
+  name: string;
+  email: string;
+}
 
 interface JournalEntry {
-    id: string,
-    title: string,
-    markdown: string
+  id: string;
+  title: string;
+  markdown: string;
 }
